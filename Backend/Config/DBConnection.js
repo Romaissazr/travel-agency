585e5b768bc27cd88783f605e7f4f8c1cdf4b606
Romaissa Zaoui <133107892+Romaissazr@users.noreply.github.com>
2025-02-05 17:06:19 +0100

    Update DBConnection.js

--- Backend/Config/DBConnection.js
+++ Backend/Config/DBConnection.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
 
-const DBConnection = async () => {
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected successfully');
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-  
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(' MongoDB connected successfully!');
+  } catch (error) {
+    console.error(' MongoDB connection error:', error.message);
+    process.exit(1);
   }
 };
 
-module.exports = DBConnection;
+module.exports = connectDB;
